c73afef6ead5e63198f69356faa470051649c79d
mishithaverma <vermamishitha@gmail.com>
2024-09-23 22:02:37 +0530

    Removed submodule reference

---
+++ 100daysofcode/Day1.py
@@ -0,0 +1,29 @@
+print("Hello Replit")
+print("how are you?")
+print("😊")
+
+print("""
+here is an exact
+      BIT
+        OF
+          TEXT
+""")
+
+print("what could go wrong")
+
+print ("hello again")
+
+print("Please work")
+
+print("= MUSIC+ =")
+print("> Songs")
+print("> Albums")
+print("> Artists")
+
+print("Mishitha")
+print("September 23th 2024")
+print("""
+I am signing up for the Replit's 100 days of Python challenge!
+I will make sure to spend some time everyday coding along, for a minimum of 10 minutes a day. I'll be using Replit, and amazing online IDE so I can do this from my phone wherever I happen to be. No excuses for not coding from the middle of a field!!
+""")
+print("I am feeling 😋")
